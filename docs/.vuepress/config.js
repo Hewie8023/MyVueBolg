900524ca3599705dd207ba59908bdb027af5ad31
Hewie8023 <Hewie8023@hotmail.com>
2019-10-12 01:55:36 +0800

    01

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    title: 'Hewie Blog',
+    description: '计算机校招面试知识梳理',
+    locales: {
+      '/': {
+        lang: 'zh-CN', 
+      }
+    },
+    head: [
+      ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    base: '/MyVueBolg/',
+    themeConfig: {
+      lastUpdated: 'Last Updated', 
+      repo: 'Hewie8023/MyVueBolg',
+      editLinks: true,
+      editLinkText: '帮助我们改善此页面！',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+        {
+          text: 'Languages',
+          ariaLabel: 'Language Menu',
+          items: [
+            { text: 'Chinese', link: '/language/chinese/' },
+            { text: 'Japanese', link: '/language/japanese/' },
+            { text: 'Group1', items: [
+              { text: 'Chinese', link: '/language/chinese/' },
+              { text: 'Japanese', link: '/language/japanese/' },
+            ] }
+          ]
+        }
+      ],
+      sidebar: [
+          '/',
+          '/network/',
+          ['/about/', '关于']
+        ]
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /favicon.ico
+heroText: Hewie Blog
+tagline: 种一棵树最好的时间是十年前，其次就是现在。
+actionText: 开始阅读 →
+actionLink: /zh/guide/
+features:
+- title: 前端进阶积累
+  details: 在纷乱的前端娱乐圈，专注js基础，打磨核心竞争力，一通百通，无惧工具/框架变迁。
+- title: 面试
+  details: 校招面试一网打尽
+- title: 随手记
+  details: 不仅仅局限于技术，也可以是平日琐碎思考的汇总、对某一项前沿技术的看法等等（正经脸）
+footer: MIT Licensed | Copyright © 2018-present Hewie
+---
